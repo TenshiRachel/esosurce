676a08c35993457e6db0909f9859e163a66854ea
james <jamestayjiayi@gmail.com>
2020-11-06 11:11:34 +0800

    fix job class

--- Esource/BL/jobs/Jobs.cs
+++ Esource/BL/jobs/Jobs.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using Esource.DAL.jobs;
 
-namespace Esource.BL.Jobs
+namespace Esource.BL.jobs
 {
     public class Jobs
     {
@@ -19,49 +19,49 @@ namespace Esource.BL.Jobs
         public string status { get; set; }
         public string remarks { get; set; }
         public decimal price { get; set; }
-    }
 
-    public Jobs()
-    {
+        public Jobs()
+        {
 
-    }
+        }
 
-    public Jobs(int Id = -1, int cid, int uid, int sid, string date_created, string sName, string cName, string username, string status = "pending", string remarks, decimal price)
-    {
-        this.Id = Id;
-        this.cid = cid;
-        this.uid = uid;
-        this.sid = sid;
-        this.date_created = date_created;
-        this.sName = sName;
-        this.cName = cName;
-        this.username = username;
-        this.status = status;
-        this.remarks = remarks;
-        this.price = price;
-    }
+        public Jobs(int cid, int uid, int sid, string date_created, string sName, string cName, string username, string remarks, decimal price, string status = "pending", int Id = -1)
+        {
+            this.Id = Id;
+            this.cid = cid;
+            this.uid = uid;
+            this.sid = sid;
+            this.date_created = date_created;
+            this.sName = sName;
+            this.cName = cName;
+            this.username = username;
+            this.status = status;
+            this.remarks = remarks;
+            this.price = price;
+        }
 
-    public int AddJob()
-    {
-        int result = new JobsDAO().Insert(this);
-        return result;
-    }
+        public int AddJob()
+        {
+            int result = new JobsDAO().Insert(this);
+            return result;
+        }
 
-    public List<Jobs> SelectByUid(string uid)
-    {
-        List<Jobs> jobs = new JobsDAO().SelectByUid(uid);
-        return jobs;
-    }
+        public List<Jobs> SelectByUid(string uid)
+        {
+            List<Jobs> jobs = new JobsDAO().SelectByUid(uid);
+            return jobs;
+        }
 
-    public List<Jobs> SelectByCid(string cid)
-    {
-        List<Jobs> jobs = new JobsDAO().SelectByCid(cid);
-        return jobs;
-    }
+        public List<Jobs> SelectByCid(string cid)
+        {
+            List<Jobs> jobs = new JobsDAO().SelectByCid(cid);
+            return jobs;
+        }
 
-    public int UpdateStatus(int id, string status)
-    {
-        int result = new JobsDAO().UpdateStatus(id, status);
-        return result;
+        public int UpdateStatus(string id, string status)
+        {
+            int result = new JobsDAO().UpdateStatus(id, status);
+            return result;
+        }
     }
 }
--- Esource/DAL/jobs/JobsDAO.cs
+++ Esource/DAL/jobs/JobsDAO.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using Esource.BL.jobs;
 
-namespace Esource.DAL
+namespace Esource.DAL.jobs
 {
     public class JobsDAO
     {
@@ -51,23 +54,24 @@ namespace Esource.DAL
             da.Fill(ds);
             int rec_cnt = ds.Tables[0].Rows.Count;
 
-            Service obj = null;
+            Jobs obj = null;
             List<Jobs> jobs = new List<Jobs>();
             if (rec_cnt > 0)
             {
                 for (int i = 0; i < rec_cnt; i++)
                 {
                     DataRow row = ds.Tables[0].Rows[i];
-                    int cid = int.Parse(row[cid].ToString());
-                    int sid = int.Parse(row[sid].ToString());
-                    string date_created = row[date_created].ToString();
-                    string sName = row[sName].ToString();
-                    string cName = row[cName].ToString();
-                    string username = row[username].ToString();
-                    string status = row[status].ToString();
-                    string remarks = row[remarks].ToString();
-                    decimal price = decimal.Parse(row[price].ToString());
-                    obj = new Jobs(int.Parse(cid), int.Parse(sid), date_created, sName, cName, username, status, remarks, decimal.Parse(price));
+                    int cid = int.Parse(row["cid"].ToString());
+                    int sid = int.Parse(row["sid"].ToString());
+                    string date_created = row["date_created"].ToString();
+                    string sName = row["sName"].ToString();
+                    string cName = row["cName"].ToString();
+                    string username = row["username"].ToString();
+                    string status = row["status"].ToString();
+                    string remarks = row["remarks"].ToString();
+                    decimal price = decimal.Parse(row["price"].ToString());
+                    int Id = int.Parse(row["Id"].ToString());
+                    obj = new Jobs(cid, int.Parse(uid), sid, date_created, sName, cName, username, remarks, price, status, Id);
                     jobs.Add(obj);
                 }
             }
@@ -89,23 +93,24 @@ namespace Esource.DAL
             da.Fill(ds);
             int rec_cnt = ds.Tables[0].Rows.Count;
 
-            Service obj = null;
+            Jobs obj = null;
             List<Jobs> jobs = new List<Jobs>();
             if (rec_cnt > 0)
             {
                 for (int i = 0; i < rec_cnt; i++)
                 {
                     DataRow row = ds.Tables[0].Rows[i];
-                    int uid = int.Parse(row[uid].ToString());
-                    int sid = int.Parse(row[sid].ToString());
-                    string date_created = row[date_created].ToString();
-                    string sName = row[sName].ToString();
-                    string cName = row[cName].ToString();
-                    string username = row[username].ToString();
-                    string status = row[status].ToString();
-                    string remarks = row[remarks].ToString();
-                    decimal price = decimal.Parse(row[price].ToString());
-                    obj = new Jobs(int.Parse(uid), int.Parse(sid), date_created, sName, cName, username, status, remarks, decimal.Parse(price));
+                    int uid = int.Parse(row["uid"].ToString());
+                    int sid = int.Parse(row["sid"].ToString());
+                    string date_created = row["date_created"].ToString();
+                    string sName = row["sName"].ToString();
+                    string cName = row["cName"].ToString();
+                    string username = row["username"].ToString();
+                    string status = row["status"].ToString();
+                    string remarks = row["remarks"].ToString();
+                    decimal price = decimal.Parse(row["price"].ToString());
+                    int Id = int.Parse(row["Id"].ToString());
+                    obj = new Jobs(int.Parse(cid), uid, sid, date_created, sName, cName, username, remarks, price, status, Id);
                     jobs.Add(obj);
                 }
             }
