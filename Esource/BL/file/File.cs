4d88d9be97ed966b44d34e7995cd88bd2a28ad6d
Anthony <joshdebean@gmail.com>
2020-11-11 22:16:37 +0800

    add file class

---
+++ Esource/BL/file/File.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Esource.DAL.file;
+
+namespace Esource.BL.file
+{
+    public class File
+    {
+        public string fileName { get; set; }
+        public string fullPath { get; set; }
+        public string type { get; set; }
+        public decimal size { get; set; }
+        public int shareId { get; set; }
+        public int uid { get; set; }
+        public int Id { get; set; }
+
+        public File()
+        {
+
+        }
+
+        public File(string fileName, string fullPath, string type, decimal size, int shareId, int uid, int Id = -1)
+        {
+            this.fileName = fileName;
+            this.fullPath = fullPath;
+            this.type = type;
+            this.size = size;
+            this.shareId = shareId;
+            this.uid = uid;
+            this.Id = Id;
+        }
+
+        public int AddFile()
+        {
+            int result = new FileDAO().Insert(this);
+            return result;
+        }
+
+        public List<File> SelectByUid(string uid)
+        {
+            List<File> files = new FileDAO().SelectByUid(uid);
+            return files;
+        }
+
+        public List<File> SelectByShare(string shareId)
+        {
+            List<File> files = new FileDAO().SelectByShare(shareId);
+            return files;
+        }
+
+        public int Remove(string Id)
+        {
+            int result = new FileDAO().Delete(Id);
+            return result;
+        }
+    }
+}
---
+++ Esource/DAL/file/FileDAO.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using Esource.BL.file;
+
+namespace Esource.DAL.file
+{
+    public class FileDAO
+    {
+        public int Insert(File file)
+        {
+            string DBConnect = ConfigurationManager.ConnectionStrings["ConnStr"].ConnectionString;
+            SqlConnection conn = new SqlConnection(DBConnect);
+
+            string sqlStmt = "INSERT INTO File (fileName, fullPath, type, size, shareId, uid)" +
+                "VALUES (@paraName, @paraPath, @paraType, @paraSize, @paraShareid, @paraUid)";
+
+            int result = 0;
+            SqlCommand sqlCmd = new SqlCommand(sqlStmt, conn);
+
+            sqlCmd.Parameters.AddWithValue("@paraName", file.fileName);
+            sqlCmd.Parameters.AddWithValue("@paraPath", file.fullPath);
+            sqlCmd.Parameters.AddWithValue("@paraType", file.type);
+            sqlCmd.Parameters.AddWithValue("@paraSize", file.size);
+            sqlCmd.Parameters.AddWithValue("@paraShareId", file.shareId);
+            sqlCmd.Parameters.AddWithValue("@paraUid", file.uid);
+
+            conn.Open();
+            result = sqlCmd.ExecuteNonQuery();
+            conn.Close();
+
+            return result;
+        }
+
+        public List<File> SelectByUid(string uid)
+        {
+            string DBConnect = ConfigurationManager.ConnectionStrings["ConnStr"].ConnectionString;
+            SqlConnection conn = new SqlConnection(DBConnect);
+
+            string sqlStmt = "SELECT * FROM File WHERE uid=@paraUid";
+
+            SqlDataAdapter da = new SqlDataAdapter(sqlStmt, conn);
+            da.SelectCommand.Parameters.AddWithValue("@paraUid", uid);
+
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            int rec_cnt = ds.Tables[0].Rows.Count;
+
+            File obj = null;
+            List<File> files = new List<File>();
+            if (rec_cnt > 0)
+            {
+                for (int i = 0; i < rec_cnt; i++)
+                {
+                    DataRow row = ds.Tables[0].Rows[i];
+                    string name = row["fileName"].ToString();
+                    string path = row["fullPath"].ToString();
+                    string type = row["type"].ToString();
+                    decimal size = decimal.Parse(row["size"].ToString());
+                    int shareId = int.Parse(row["shareId"].ToString());
+                    int Id = int.Parse(row["Id"].ToString());
+                    obj = new File(name, path, type, size, shareId,int.Parse(uid), Id);
+                    files.Add(obj);
+                }
+            }
+
+            return files;
+        }
+
+        public List<File> SelectByShare(string shareId)
+        {
+            string DBConnect = ConfigurationManager.ConnectionStrings["ConnStr"].ConnectionString;
+            SqlConnection conn = new SqlConnection(DBConnect);
+
+            string sqlStmt = "SELECT * FROM File WHERE shareId=@paraSid";
+
+            SqlDataAdapter da = new SqlDataAdapter(sqlStmt, conn);
+            da.SelectCommand.Parameters.AddWithValue("@paraSid", shareId);
+
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            int rec_cnt = ds.Tables[0].Rows.Count;
+
+            File obj = null;
+            List<File> files = new List<File>();
+            if (rec_cnt > 0)
+            {
+                for (int i = 0; i < rec_cnt; i++)
+                {
+                    DataRow row = ds.Tables[0].Rows[i];
+                    string name = row["fileName"].ToString();
+                    string path = row["fullPath"].ToString();
+                    string type = row["type"].ToString();
+                    decimal size = decimal.Parse(row["size"].ToString());
+                    int uid = int.Parse(row["uid"].ToString());
+                    int Id = int.Parse(row["Id"].ToString());
+                    obj = new File(name, path, type, size, int.Parse(shareId), uid, Id);
+                    files.Add(obj);
+                }
+            }
+
+            return files;
+        }
+
+        public int Delete(string Id)
+        {
+            string DBConnect = ConfigurationManager.ConnectionStrings["ConnStr"].ConnectionString;
+            SqlConnection conn = new SqlConnection(DBConnect);
+
+            string sqlStmt = "DELETE FROM File WHERE Id=@paraId";
+
+            int result = 0;
+            SqlCommand sqlCmd = new SqlCommand(sqlStmt, conn);
+
+            sqlCmd.Parameters.AddWithValue("@paraId", Id);
+
+            conn.Open();
+            result = sqlCmd.ExecuteNonQuery();
+            conn.Close();
+
+            return result;
+        }
+    }
+}
