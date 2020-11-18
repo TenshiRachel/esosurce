e1248c89e418c69227a5aafbd2a8965cc84b2fec
Anthony <joshdebean@gmail.com>
2020-11-18 18:22:43 +0800

    add transaction classes

---
+++ Esource/BL/service/Transaction.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Esource.DAL.service;
+
+namespace Esource.BL.service
+{
+    public class Transaction
+    {
+        public string serviceProvider { get; set; }
+        public string service { get; set; }
+        public string currency { get; set; }
+        public decimal price { get; set; }
+        public string date { get; set; }
+        public int uid { get; set; }
+        public int Id { get; set; }
+
+        public Transaction()
+        {
+
+        }
+
+        public Transaction(string serviceProvider, string service, string currency, decimal price, string date, int uid, int Id = -1)
+        {
+            this.serviceProvider = serviceProvider;
+            this.service = service;
+            this.currency = currency;
+            this.price = price;
+            this.date = date;
+            this.uid = uid;
+            this.Id = Id;
+        }
+
+        public int AddTrans()
+        {
+            int result = new TransactionDAO().Insert(this);
+            return result;
+        }
+
+        public List<Transaction> SelectByUid(string uid)
+        {
+            List<Transaction> trans = new TransactionDAO().SelectByUid(uid);
+            return trans;
+        }
+    }
+}
---
+++ Esource/DAL/service/TransactionDAO.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using Esource.BL.service;
+
+namespace Esource.DAL.service
+{
+    public class TransactionDAO
+    {
+        public int Insert(Transaction trans)
+        {
+            string DBConnect = ConfigurationManager.ConnectionStrings["ConnStr"].ConnectionString;
+            SqlConnection conn = new SqlConnection(DBConnect);
+
+            string sqlStmt = "INSERT INTO Transaction (serviceProvider, service, currency, price, date, uid)" +
+                "VALUES (@paraFreelance, @paraService, @paraCurr, @paraPrice, @paraDate, @paraUid)";
+
+            int result = 0;
+            SqlCommand sqlCmd = new SqlCommand(sqlStmt, conn);
+
+            sqlCmd.Parameters.AddWithValue("@paraFreelance", trans.serviceProvider);
+            sqlCmd.Parameters.AddWithValue("@paraService", trans.service);
+            sqlCmd.Parameters.AddWithValue("@paraCurr", trans.currency);
+            sqlCmd.Parameters.AddWithValue("@paraPrice", trans.price);
+            sqlCmd.Parameters.AddWithValue("@paraDate", trans.date);
+            sqlCmd.Parameters.AddWithValue("@paraUid", trans.uid);
+
+            conn.Open();
+            result = sqlCmd.ExecuteNonQuery();
+            conn.Close();
+
+            return result;
+        }
+
+        public List<Transaction> SelectByUid(string uid)
+        {
+            string DBConnect = ConfigurationManager.ConnectionStrings["ConnStr"].ConnectionString;
+            SqlConnection conn = new SqlConnection(DBConnect);
+
+            string sqlStmt = "SELECT * FROM Transaction WHERE uid=@paraUid";
+
+            SqlDataAdapter da = new SqlDataAdapter(sqlStmt, conn);
+            da.SelectCommand.Parameters.AddWithValue("@paraUid", uid);
+
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            int rec_cnt = ds.Tables[0].Rows.Count;
+
+            Transaction obj = null;
+            List<Transaction> trans = new List<Transaction>();
+            if (rec_cnt > 0)
+            {
+                for (int i = 0; i < rec_cnt; i++)
+                {
+                    DataRow row = ds.Tables[0].Rows[i];
+                    string serviceProvider = row["serviceProvider"].ToString();
+                    string service = row["service"].ToString();
+                    string curr = row["currency"].ToString();
+                    decimal price = decimal.Parse(row["price"].ToString());
+                    string date = row["date"].ToString();
+                    int Id = int.Parse(row["Id"].ToString());
+
+                    obj = new Transaction(serviceProvider, service, curr, price, date, int.Parse(uid), Id);
+                    trans.Add(obj);
+                }
+            }
+
+            return trans;
+        }
+    }
+}
